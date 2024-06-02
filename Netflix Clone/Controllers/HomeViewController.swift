8492ea17961cf21f1316d1d8fb3c385dcb170821
Enrique Sarmiento <jeisone.sarmiento@gmail.com>
2024-06-02 10:01:28 -0400

    FIXED: changes on table view header for the main screen

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -8,39 +8,65 @@
 import UIKit
 
 class HomeViewController: UIViewController{
-
+   
+   let sectionTitles : [String] = [
+   "Trending Movies", "Popular",
+   "Trending TV", "Top Rated", "Upcoming Movies"
+   ]
+   
    //create a table for this view controller with our custom cell
    private let homeFeedtable: UITableView = {
       let table = UITableView(frame: .zero, style: .grouped)
       table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
       return table
    }()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-       view.backgroundColor = .systemBackground
-       view.addSubview(homeFeedtable)
-       // added protocols for table
-       homeFeedtable.delegate = self
-       homeFeedtable.dataSource = self
-       
-       homeFeedtable.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
-       
-    }
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      
+      view.backgroundColor = .systemBackground
+      view.addSubview(homeFeedtable)
+      // added protocols for table
+      homeFeedtable.delegate = self
+      homeFeedtable.dataSource = self
+      
+      configureNavBar()
+      
+      let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+      //       homeFeedtable.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+      homeFeedtable.tableHeaderView = headerView
+      
+   }
+   
+   private func configureNavBar(){
+      var image = UIImage(named: "netflix-logo")
+      image = image?.withRenderingMode(.alwaysOriginal)
+      
+      navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .done, target: self, action: nil)
+      
+      
+      navigationItem.rightBarButtonItems = [
+         UIBarButtonItem(image: UIImage(systemName: "person"), style: .done, target: self, action: nil),
+         UIBarButtonItem(image: UIImage(systemName: "play.rectangle"), style: .done, target: self, action: nil)
+      ]
+      
+      navigationController?.navigationBar.tintColor = .white
+      
+      
+   }
    
    // here we load our table as a subview
    override func viewDidLayoutSubviews() {
       super.viewDidLayoutSubviews()
       homeFeedtable.frame = view.bounds
    }
-
+   
 }
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource  {
    
    func numberOfSections(in tableView: UITableView) -> Int {
-      return 20
+      return sectionTitles.count
    }
    
    //here we defined the numbers of rows for our cell
@@ -66,5 +92,28 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource  {
    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
       40
    }
+   
+   func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+      guard let header = view as? UITableViewHeaderFooterView else {return}
+      header.textLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
+      header.textLabel?.frame = CGRect(x: header.bounds.origin.x + 20, y: header.bounds.origin.y, width: 100, height: header.bounds.height)
+      header.textLabel?.textColor = .white
+      header.textLabel?.text = header.textLabel?.text?.lowercased()
+   }
+      
+   
+   //here we assigned the titles for each sections
+   func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+      return sectionTitles[section]
+   }
+   
+   func scrollViewDidScroll(_ scrollView: UIScrollView) {
+      let defaultOffset = view.safeAreaInsets.top
+      let offset = scrollView.contentOffset.y + defaultOffset
+      
+      navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
+   }
+   
+   
    
 }
