c695ebeb53a8f05bb4e74b0323bbf899c9e09624
Enrique Sarmiento <jeisone.sarmiento@gmail.com>
2024-06-02 17:32:37 -0400

    FAETURE: upcoming view controller

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by Enrique Sarmiento on 6/2/24.
+//
+
+import Foundation
+
+struct TitleViewModel {
+   let titleName: String
+   let posterURL: String
+}
